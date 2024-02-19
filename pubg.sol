d66be662d78c2214a920b74da8f8f2266500a04c
Nitish7545 <160515639+Nitish7545@users.noreply.github.com>
2024-02-20 00:23:13 +0530

    Update pubg.sol

--- pubg.sol
+++ pubg.sol
@@ -1,1 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PUBGToken is ERC20("PUBGToken", "PUBG"), Ownable {
+    uint256 public healthKits = 10;
+    mapping(address => uint256) public killCount;
+    mapping(address => bool) public rewardsRedeemed;
+
+    constructor() {
+        _mint(msg.sender, healthKits);
+    }
+
+    function collectHealthKits(uint256 kits) public {
+        _mint(msg.sender, kits);
+        healthKits += kits;
+    }
+
+    function shootEnemies(uint256 enemies) public {
+        require(healthKits > 0, "Not enough health kits");
+        require(enemies > 0, "Number of enemies shot must be greater than 0");
+
+        healthKits -= enemies;
+        killCount[msg.sender] += enemies;
+    }
+
+    function issueRewards(address addr, uint256 rewards) public onlyOwner {
+        rewardsRedeemed[addr] = false;
+        _mint(addr, rewards);
+    }
+
+    function redeemRewards() public {
+        require(!rewardsRedeemed[msg.sender], "Rewards already redeemed");
+        rewardsRedeemed[msg.sender] = true;
+    }
+}
